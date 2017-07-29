bb006c487d517dba84e04ffd3bd531ebba49731f
TheMaxSinger <prs22silver@gmail.com>
2017-07-29 08:29:29 +0700

    full sentence

--- src/iot/pi/queue/domain/DigitAnnouncer.java
+++ src/iot/pi/queue/domain/DigitAnnouncer.java
@@ -18,5 +18,22 @@ public class DigitAnnouncer implements Announcer {
 			Pause.delay(0.1);
 		}
 	}
+	
+	public void announce(List<Announceable> announceables, int slot) throws MalformedURLException { 
+		SoundPlayer.play(QueueVoices.NUMBER.getAudioFile());
+		announce(announceables);
+		SoundPlayer.play(QueueVoices.DEQUEUE.getAudioFile());
+		Pause.delay(0.1);
+		SoundPlayer.play(QueueVoices.NUMBER.getAudioFile());
+		Pause.delay(0.1);
+		switch (slot) { 
+			case 1: SoundPlayer.play(QueueVoices.NEUNG.getAudioFile()); break;
+			case 2: SoundPlayer.play(QueueVoices.SONG.getAudioFile()); break;
+			case 3: SoundPlayer.play(QueueVoices.SARM.getAudioFile()); break;
+			default: break;
+		}
+		Pause.delay(0.1);
+		SoundPlayer.play(QueueVoices.KAA.getAudioFile());
+	}
 
 }
