5ae026676dc4a2f1156dca5c6123e3032bb2c69c
Max <bpakawat@scan-it.com.sg>
2017-09-22 10:08:57 +0700

    add db class

---
+++ src/main/java/iot/pi/queue/util/DBUtil.java
@@ -0,0 +1,33 @@
+package iot.pi.queue.util;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	public static void main(String[] argv) {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/iot", "max", "isaac34");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		}
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
+
+}
