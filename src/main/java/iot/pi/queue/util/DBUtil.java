ad327491dae202bf75bc1d596c066325b3e1fc5e
Max <bpakawat@scan-it.com.sg>
2017-09-27 11:54:03 +0700

    DB

--- src/main/java/iot/pi/queue/util/DBUtil.java
+++ src/main/java/iot/pi/queue/util/DBUtil.java
@@ -2,6 +2,7 @@ package iot.pi.queue.util;
 
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -33,10 +34,48 @@ public class DBUtil {
 		}
 	}
 	
+	private static void closeResultSet(ResultSet resultSet) { 
+		try {
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void resetQueue() throws SQLException { 
 		Connection connection = getConnection();
 		String updateTableSQL = "UPDATE queue SET called = 'N', finished = 'N', queue = 0";
 		PreparedStatement preparedStatement = connection.prepareStatement(updateTableSQL);
+		preparedStatement.executeUpdate();
+		closeConnection(connection);
+	}
+	
+	public static void updateQueue(int slot) throws SQLException { 
+		int max = 0;
+		Connection connection = getConnection();
+		String selectMaxQueue = "SELECT MAX(queue) AS max_queue FROM queue";
+		PreparedStatement statement = connection.prepareStatement(selectMaxQueue);
+		ResultSet result = statement.executeQuery();
+		while(result.next()) {
+		    max = result.getInt("max_queue");
+		}
+		if (max > 999) { 
+			max = 0;
+		}
+		closeResultSet(result);
+		String updateTableSQL = "UPDATE queue SET called = 'Y', finished = 'N', queue = ? WHERE slot = ?";
+		PreparedStatement preparedStatement = connection.prepareStatement(updateTableSQL);
+		preparedStatement.setInt(1, ++max);
+		preparedStatement.setInt(2, slot);
+		preparedStatement.executeUpdate();
+		closeConnection(connection);
+	}
+	
+	public static void terminateQueue(int slot) throws SQLException { 
+		Connection connection = getConnection();
+		String updateTableSQL = "UPDATE queue SET finished = 'Y' WHERE slot = ?";
+		PreparedStatement preparedStatement = connection.prepareStatement(updateTableSQL);
+		preparedStatement.setInt(1, slot);
 		preparedStatement.executeUpdate();
 		closeConnection(connection);
 	}
